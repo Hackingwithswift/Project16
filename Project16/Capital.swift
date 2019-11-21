a5ac1c77854ed38570e0f313bb11b5d33144562b
veerababum <veerababum@users.noreply.github.com>
2019-11-21 14:36:55 -0800

    MKMapView, MkAnnotation implemented

---
+++ Project16/Capital.swift
@@ -0,0 +1,25 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by vmulugu on 11/21/19.
+//  Copyright © 2019 vmulugu. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+      var coordinate: CLLocationCoordinate2D
+      var info: String
+    
+     init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+    
+    
+
+}
